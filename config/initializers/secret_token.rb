c7252e115e8a755e9c7944440f63c0cf06363830
mkeung <mikekeung@gmail.com>
2013-03-03 02:15:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auhack::Application.config.secret_token = '1ab7953105fd0c55a4e01dcf75c29992354fc67b95237a9694aef0b1603a55baa1fdf2fa1e88f0a9263b427650cb54d4f3f6844898b0b369b0a6e97b98225819'
