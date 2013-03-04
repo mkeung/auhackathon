c5f95ec134ed70d16eef48f90884ea58ec1fd5f0
mkeung <mikekeung@gmail.com>
2013-03-03 21:33:54 -0500

    s3 setup

--- config/application.rb
+++ config/application.rb
@@ -60,3 +60,19 @@ module Auhack
     config.assets.version = '1.0'
   end
 end
+
+Spud::Photos.configure do |config|
+  config.base_layout = 'application'
+  config.galleries_enabled = false
+  config.base_path = 'photos'
+  config.photo_styles = {
+    :small => '50x50#',
+    :medium => '200x200#',
+    :large => '400x400#',
+    :huge => '600x600'
+  }
+  config.paperclip_storage = :s3 #use :s3 to use s3 storage (aws gem required)
+      config.s3_credentials = S3_CREDENTIALS
+      config.storage_path = ":rails_root/public/system/spud_photos/:id/:style/:basename.:extension"
+      config.storage_url = "/system/spud_photos/:id/:style/:basename.:extension"
+end
